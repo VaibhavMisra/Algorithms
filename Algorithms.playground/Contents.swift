203de521345ac668fe9525a3c9014e3d68c560bd
Vaibhav <vaibhavmisra.1987@gmail.com>
2019-12-23 08:19:00 +0530

    Is Palindrome, for only alphanumeric.

--- Algorithms.playground/Contents.swift
+++ Algorithms.playground/Contents.swift
@@ -1,5 +1,27 @@
 import UIKit
 
+//https://leetcode.com/problems/valid-palindrome/
+func isPalindrome(_ s: String) -> Bool {
+    guard s.isEmpty == false, s.count != 1 else { return true }
+    
+    var input = s
+    let unsafeChars = CharacterSet.alphanumerics.inverted
+    input  = input.components(separatedBy: unsafeChars).joined(separator: "").lowercased()
+    let inputArray = Array(input)
+    var i = 0
+    var j = inputArray.count - 1
+    while (i < j) {
+        if(inputArray[i] != inputArray[j]) {
+            return false
+        }
+        i += 1
+        j -= 1
+    }
+    return true
+}
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
+print(isPalindrome("race a car"))
 
 //https://leetcode.com/problems/contains-duplicate/
 func containsDuplicate(_ nums: [Int]) -> Bool {
